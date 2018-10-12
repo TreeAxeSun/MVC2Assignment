221b1d9829dc4e94faf64af77d96f69d139a868c
YoungSeok-Ahn <veris1975@outlook.com>
2018-10-12 10:09:31 -0500

    Add project files.

---
+++ Assignment1012/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1012.Startup))]
+namespace Assignment1012
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
